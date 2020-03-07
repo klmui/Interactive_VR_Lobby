21916f2f9da70917fabc39e2e4ccb98e651391bf
Kenny Mui <klmui@wisc.edu>
2020-03-06 23:22:25 -0600

    Change script text color

--- Assets/Scripts/MaterialGradientModifier.cs
+++ Assets/Scripts/MaterialGradientModifier.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Renderer))] // Tells Unity that this requires a Renderer, safety
 public class MaterialGradientModifier : MonoBehaviour
 {
+    Renderer _renderer;
+
+    // Called before start
+    void Awake() {
+        // Find first renderer that is in same game object
+        _renderer = GetComponent<Renderer>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _renderer.material.color = Color.red;
     }
 
     // Update is called once per frame
