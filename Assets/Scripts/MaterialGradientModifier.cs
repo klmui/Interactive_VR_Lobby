84effe26123cd33df5cb29832ac600df5c9e1110
Kenny Mui <klmui@wisc.edu>
2020-03-07 01:17:26 -0600

    Modify color script

--- Assets/Scripts/MaterialGradientModifier.cs
+++ Assets/Scripts/MaterialGradientModifier.cs
@@ -1,27 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent (typeof (Renderer))] // Tells Unity that this requires a Renderer, safety
+[RequireComponent(typeof(Renderer))] // Tells Unity that this requires a Renderer, safety
 public class MaterialGradientModifier : MonoBehaviour
 {
-    Renderer _renderer;
+    private Renderer _renderer; // Don't want it to be accessable to other scripts
+
+    // public Color myColor;
+    [SerializeField] private Gradient gradient; // Tell Unity to serialize field, only available from Unity
+
+    // Disappear from inspector if no public
+    float _gradientPosition;
+    public float gradientPosition
+    {
+        get { return _gradientPosition; }
+        set
+        {
+            if (_gradientPosition != value)
+            {
+                _gradientPosition = value;
+                _renderer.material.color = gradient.Evaluate(_gradientPosition);
+            }
+        }
+    }
+
+    //void SetGradientPosition (float position)
+    //{
+    //    if (position == gradientPosition)
+    //    {
+    //        return;
+    //    } else
+    //    {
+    //        gradientPosition = position;
+    //        _renderer.material.color = gradient.Evaluate(gradientPosition);
+    //    }
+    //}
 
     // Called before start
-    void Awake() {
+    private void Awake()
+    {
         // Find first renderer that is in same game object
         _renderer = GetComponent<Renderer>();
     }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        _renderer.material.color = Color.red;
+        //SetGradientPosition(0);
+        gradientPosition = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private void Update()
+    //{
+    //    // Current time of the game and sin goes from -1 to 1
+    //    //SetGradientPosition(Mathf.Sin((Time.time) * 0.5f) + 0.5f);
+    //    gradientPosition = Mathf.Sin((Time.time) * 0.5f) + 0.5f;
+    //}
 }
