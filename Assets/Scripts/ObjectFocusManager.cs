1a7f7daf9be370ae88e3ca1f213f0a0725cd670d
Kenny Mui <klmui@wisc.edu>
2020-03-21 15:58:08 -0500

    Implement manager as singleton

---
+++ Assets/Scripts/ObjectFocusManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocusManager : MonoBehaviour
+{
+
+    List<ObjectFocus> objectsInRange = new List<ObjectFocus>();
+
+    #region Singleton
+
+    private static ObjectFocusManager _instance;
+    public static ObjectFocusManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ObjectFocusManager>();
+
+                if (_instance == null)
+                {
+                    _instance = (new GameObject("ObjectFocusManager : Singleton")).AddComponent<ObjectFocusManager>();
+                }
+            }
+            return _instance;
+        }
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    #endregion
+
+    #region MonoBehaviour
+    private void Awake()
+    {
+        // Prevent duplicate instances of this game object
+        if (Instance && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+
+    private void OnDisable()
+    {
+        Instance = null;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+}
